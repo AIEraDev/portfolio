97bf86ad6b60f1b512ee47f1b2b7186d3fe517cd
AIEraDev <musaabdulkabeer19@gmail.com>
2026-02-01 21:38:38 +0100

    Update resume link to Google Docs and remove internal resume page

--- src/components/ui/word-rotate.tsx
+++ src/components/ui/word-rotate.tsx
@@ -4,41 +4,29 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 
 interface WordRotateProps {
-    words: string[];
-    className?: string;
-    duration?: number;
+  words: string[];
+  className?: string;
+  duration?: number;
 }
 
-export function WordRotate({
-    words,
-    className,
-    duration = 2500,
-}: WordRotateProps) {
-    const [index, setIndex] = useState(0);
+export function WordRotate({ words, className, duration = 2500 }: WordRotateProps) {
+  const [index, setIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setIndex((prevIndex) => (prevIndex + 1) % words.length);
-        }, duration);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, duration);
 
-        return () => clearInterval(interval);
-    }, [words, duration]);
+    return () => clearInterval(interval);
+  }, [words, duration]);
 
-    return (
-        <span className={cn("inline-grid grid-cols-1 text-left", className)}>
-            {words.map((word, i) => (
-                <span
-                    key={word}
-                    className={cn(
-                        "col-start-1 row-start-1 transition-all duration-700 ease-in-out",
-                        i === index
-                            ? "opacity-100 translate-y-0"
-                            : "opacity-0 translate-y-4 pointer-events-none absolute"
-                    )}
-                >
-                    {word}
-                </span>
-            ))}
+  return (
+    <span className={cn("inline-grid grid-cols-1 text-left ", className)}>
+      {words.map((word, i) => (
+        <span key={word} className={cn("col-start-1 row-start-1 transition-all duration-700 ease-in-out whitespace-nowrap", i === index ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4 pointer-events-none absolute")}>
+          {word}
         </span>
-    );
+      ))}
+    </span>
+  );
 }
